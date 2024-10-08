579cd1b73410e85db3737d8bfbb33fb33defa880
Jonathan Hartley <tartley@tartley.com>
2024-10-07 19:40:57 -0500

    Fix pythonstartup script to play nice with Python3.13
    1. Python3.13 automatically persists REPL command history. No need for
    us to do it except for versions <=3.12. Tweak the filename we use for
    those older Pythons, to match the name used by 3.13.
    
    2. Tweak ps1,ps2 to declare characters after the prompt as visible.
    Without this, we got a weird 1 char offset between the text cursor and
    the place we were actually typing.
    
    3. Refactor out a function 'msg' to display message to user.

--- .pythonstartup.py
+++ .pythonstartup.py
@@ -13,9 +13,15 @@ true = True
 false = False
 null = None
 
+
+def msg(message):
+    print(f"{os.path.basename(__file__)}: {message}", file=sys.stderr)
+
+
 try:
     import readline
 except ImportError:
+    msg("Package 'readline' not found")
     readline = None
 else:
     import rlcompleter
@@ -33,8 +39,8 @@ def set_ps1():
         bold_yellow = prefix + '1;33m'
         reset = prefix + '0m'
 
-        sys.ps1 = nonvis + bold_yellow + vis + '>>> ' + nonvis + reset
-        sys.ps2 = nonvis + bold_yellow + vis + '... ' + nonvis + reset
+        sys.ps1 = nonvis + bold_yellow + vis + '>>> ' + nonvis + reset + vis
+        sys.ps2 = nonvis + bold_yellow + vis + '... ' + nonvis + reset + vis
 
 
 def enable_tab_completion():
@@ -42,22 +48,24 @@ def enable_tab_completion():
         readline.parse_and_bind('bind ^I rl_complete')
     else:
         readline.parse_and_bind('tab: complete')
-    print(".pythonstartup.py: Bound readline 'complete' to [tab]")
+    msg("Bound readline 'complete' to [tab]")
 
 
-def read_history_file():
-    histfile = os.path.join(os.environ['HOME'], '.pythonhistory')
+def get_history_filename():
+    return os.path.join(os.environ['HOME'], '.python_history')
+
+
+def read_history_file(histfile):
     try:
         readline.read_history_file(histfile)
-        print(f".pythonstartup.py: Read history ({histfile})")
-    except IOError:
-        pass
-    return histfile
+        msg(f"Read history file '{histfile}'")
+    except IOError as exc:
+        msg(f"ERROR: Failed to read history file '{histfile}' ({exc})")
 
 
-def write_history_atexit(histfile):
+def write_history_file_atexit(histfile):
     atexit.register(readline.write_history_file, histfile)
-    print(f".pythonstartup.py: Registered to write history file atexit")
+    msg(f"Will write to history file atexit")
 
 
 def delete_new_globals(startenv):
@@ -69,8 +77,11 @@ def main():
     set_ps1()
     if readline:
         enable_tab_completion()
-        histfile = read_history_file()
-        write_history_atexit(histfile)
+        # Python3.13 began persisting REPL history automatically (to the same file we use)
+        if sys.version_info < (3, 13):
+            histfile = get_history_filename()
+            read_history_file(histfile)
+            write_history_file_atexit(histfile)
     delete_new_globals(startenv)
 
 
